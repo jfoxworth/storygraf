b71cfe3ef93d745b7cee3c6f7ffe4713633fc386
Joshua Foxworth <jfoxworth@cadwolf.com>
2022-06-30 15:26:49 -0500

    add in child tag description

--- src/components/shared/TagChildren/index.jsx
+++ src/components/shared/TagChildren/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { listTagRelations } from "../../../graphql/queries";
+import { Row, Col } from "react-bootstrap";
 import { API } from "aws-amplify";
 import Tag from "../Tag";
 
@@ -24,7 +25,26 @@ const TagChildren = ({ tag, setNumChildren = () => {} }) => {
   return (
     <div className="pt-3">
       {childData.length > 0 &&
-        childData.map((tagRel, j) => <Tag key={j} tag={tagRel.childTag} />)}
+        childData.map((tagRel, j) => {
+          let thisData = JSON.parse(tagRel.childTag.data);
+          return (
+            <Row key={`childtag${j}`}>
+              <Col xs={{ span: 6 }} md={{ span: 4 }}>
+                <div style={{ display: "inline-block" }}>
+                  <Tag tag={tagRel.childTag} />
+                </div>
+              </Col>
+              <Col xs={{ span: 6 }} md={{ span: 8 }}>
+                <div
+                  className={"text-muted"}
+                  style={{ display: "inline-block" }}
+                >
+                  {thisData.description}
+                </div>
+              </Col>
+            </Row>
+          );
+        })}
     </div>
   );
 };
