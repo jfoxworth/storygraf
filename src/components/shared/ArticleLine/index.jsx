ee8fe2c20a06dfd7bb64f18c4332928b07202dd7
Joshua Foxworth <jfoxworth@cadwolf.com>
2022-07-05 16:52:06 -0500

    change tag view to article

--- src/components/shared/ArticleLine/index.jsx
+++ src/components/shared/ArticleLine/index.jsx
@@ -42,83 +42,103 @@ const ArticleLine = ({ article, userData, showEdits = false, tag = {} }) => {
       />
 
       {as && (
-        <Row className={"mb-3"}>
-          <Col xs={{ span: 4 }} lg={{ span: 4 }}>
-            <div
-              style={{
-                minWidth: "100px",
-                backgroundImage: "url(" + article?.data?.image?.url + ")",
-                minHeight: "100px",
-                backgroundSize: "cover",
-                borderRadius: "5px 5px 5px 5px",
-              }}
-            >
-              {showEdits && (
-                <div className={"p-2 d-flex justify-content-between"}>
-                  <Button
-                    variant="outline-secondary"
-                    className="icon-button px-0 py-1 ml-5"
-                    style={{
-                      fontSize: "0.8em",
-                      width: "30px",
-                      height: "30px",
-                    }}
-                    onClick={() => deleteThisArticle(article.id)}
-                  >
-                    <BsFillTrashFill />
-                  </Button>
-                  <Button
-                    variant="outline-secondary"
-                    className="icon-button px-0 py-1 ml-5"
-                    style={{ fontSize: "0.8em", width: "30px", height: "30px" }}
-                    onClick={() => setShowEditArticle(true)}
-                  >
-                    <BsGear />
-                  </Button>
-                </div>
-              )}
-            </div>
+        <Row>
+          <Col
+            xs={{ span: 5 }}
+            lg={{ span: 4 }}
+            style={{ borderRight: "2px solid #ccc" }}
+            className={"m-0 pb-2"}
+          >
+            <DateBlock
+              datestring={article?.data?.published}
+              time={artData}
+              articleid={article?.id}
+            />
           </Col>
-
-          <Col xs={{ span: 8 }} lg={{ span: 8 }}>
-            <Row>
-              <Col xs={{ span: 10 }} md={{ span: 10 }}>
+          <Col xs={{ span: 7 }} lg={{ span: 8 }} className={"pb-3"}>
+            <Row className={"mb-3 mx-3"}>
+              <Col xs={{ span: 4 }} lg={{ span: 4 }}>
                 <div
-                  className={"text-weight-bold"}
-                  style={{ fontSize: "0.9em" }}
+                  style={{
+                    minWidth: "100px",
+                    backgroundImage: "url(" + article?.data?.image?.url + ")",
+                    minHeight: "100px",
+                    backgroundSize: "cover",
+                    borderRadius: "5px 5px 5px 5px",
+                  }}
                 >
-                  {article?.data?.title}
+                  {showEdits && (
+                    <div className={"p-2 d-flex justify-content-between"}>
+                      <Button
+                        variant="outline-secondary"
+                        className="icon-button px-0 py-1 ml-5"
+                        style={{
+                          fontSize: "0.8em",
+                          width: "30px",
+                          height: "30px",
+                        }}
+                        onClick={() => deleteThisArticle(article.id)}
+                      >
+                        <BsFillTrashFill />
+                      </Button>
+                      <Button
+                        variant="outline-secondary"
+                        className="icon-button px-0 py-1 ml-5"
+                        style={{
+                          fontSize: "0.8em",
+                          width: "30px",
+                          height: "30px",
+                        }}
+                        onClick={() => setShowEditArticle(true)}
+                      >
+                        <BsGear />
+                      </Button>
+                    </div>
+                  )}
                 </div>
               </Col>
-              <Col className={"right-text"} xs={{ span: 2 }} md={{ span: 2 }}>
-                <Source source={article?.data?.source || article?.source} />
-              </Col>
-            </Row>
-            <Row className={"my-1"}>
-              <Col xs={{ span: 6 }} md={{ span: 6 }}>
-                <DateBlock
-                  datestring={article?.data?.published}
-                  time={artData}
-                  articleid={article?.id}
-                />
-              </Col>
-            </Row>
-            <Row>
-              <div className={"text-muted"} style={{ fontSize: "0.7em" }}>
-                {article?.data?.userDescription
-                  ? article.data.userDescription
-                  : article?.data?.description}
-              </div>
-            </Row>
-            <Row className={"my-3"}>
-              <Col>
-                {article.data?.cumulatives?.map((cumItem, i) => (
-                  <Row key={`cumulative${i}`}>
-                    <div className={"text-muted"} style={{ fontSize: "0.7em" }}>
-                      {cumItem.text} - {cumItem.value}
+
+              <Col xs={{ span: 8 }} lg={{ span: 8 }}>
+                <Row className={"mb-3"}>
+                  <Col xs={{ span: 10 }} md={{ span: 10 }}>
+                    <div
+                      className={"text-weight-bold"}
+                      style={{ fontSize: "0.9em" }}
+                    >
+                      {article?.data?.title}
                     </div>
+                  </Col>
+                  <Col
+                    className={"right-text"}
+                    xs={{ span: 2 }}
+                    md={{ span: 2 }}
+                  >
+                    <Source source={article?.data?.source || article?.source} />
+                  </Col>
+                </Row>
+                <Row>
+                  <div className={"text-muted"} style={{ fontSize: "0.7em" }}>
+                    {article?.data?.userDescription
+                      ? article.data.userDescription
+                      : article?.data?.description}
+                  </div>
+                </Row>
+                {article.data?.cumulatives.length > 0 && (
+                  <Row className={"my-3"}>
+                    <Col>
+                      {article.data?.cumulatives?.map((cumItem, i) => (
+                        <Row key={`cumulative${i}`}>
+                          <div
+                            className={"text-muted"}
+                            style={{ fontSize: "0.7em" }}
+                          >
+                            {cumItem.text} - {cumItem.value}
+                          </div>
+                        </Row>
+                      ))}
+                    </Col>
                   </Row>
-                ))}
+                )}
               </Col>
             </Row>
           </Col>
