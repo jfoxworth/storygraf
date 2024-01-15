1934410df8843dcc192a7c8b80eb7b58656ff7b5
Joshua Foxworth <jfoxworth@cadwolf.com>
2024-01-15 09:49:52 -0600

    small changes

--- src/pages/login-old/index.js
+++ src/pages/login-old/index.js
@@ -343,6 +343,34 @@ const LoginPage = () => {
                   Create an account
                 </Typography>
               </Box>
+              <Divider
+                sx={{
+                  '& .MuiDivider-wrapper': { px: 4 },
+                  mt: theme => `${theme.spacing(5)} !important`,
+                  mb: theme => `${theme.spacing(7.5)} !important`
+                }}
+              >
+                or
+              </Divider>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <IconButton href='/' component={Link} sx={{ color: '#497ce2' }} onClick={e => e.preventDefault()}>
+                  <Icon icon='mdi:facebook' />
+                </IconButton>
+                <IconButton href='/' component={Link} sx={{ color: '#1da1f2' }} onClick={e => e.preventDefault()}>
+                  <Icon icon='mdi:twitter' />
+                </IconButton>
+                <IconButton
+                  href='/'
+                  component={Link}
+                  onClick={e => e.preventDefault()}
+                  sx={{ color: theme => (theme.palette.mode === 'light' ? '#272727' : 'grey.300') }}
+                >
+                  <Icon icon='mdi:github' />
+                </IconButton>
+                <IconButton href='/' component={Link} sx={{ color: '#db4437' }} onClick={e => e.preventDefault()}>
+                  <Icon icon='mdi:google' />
+                </IconButton>
+              </Box>
             </form>
           </BoxWrapper>
         </Box>
---
+++ src/utils/cognito.js
@@ -0,0 +1,135 @@
+import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js'
+
+const createPool = () => {
+  const poolData = {
+    UserPoolId: process.env.REACT_APP_UserPoolId,
+    ClientId: process.env.REACT_APP_UserPoolClientId
+  }
+  const poolDataTemp = {
+    UserPoolId: 'us-east-2_bInPwxVtq',
+    ClientId: '688p0ue75spe2edfmq26f24kfm'
+  }
+  return new AmazonCognitoIdentity.CognitoUserPool(poolDataTemp)
+}
+
+const signUpUser = (username, email, password) => {
+  var attributeList = []
+  var dataEmail = {
+    Name: 'email',
+    Value: email
+  }
+  var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail)
+  attributeList.push(attributeEmail)
+  const CognitoUserPool = createPool()
+  CognitoUserPool.signUp(
+    email,
+    password,
+    attributeList,
+    null,
+    function (err, result) {
+      if (err) {
+        alert(err.message || JSON.stringify(err))
+        return
+      } else {
+        return createDBUserData(username, email, result.userSub)
+      }
+    },
+    null
+  )
+}
+
+// Make call to add user and profile
+const createDBUserData = (username, email, id) => {
+  fetch('http://localhost:3080/api/addUser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      cognitoId: id
+    })
+  }).then(response => {
+    return true
+  })
+}
+
+const getSession = async () =>
+  await new Promise((resolve, reject) => {
+    const userPool = createPool()
+    const user = userPool.getCurrentUser()
+    if (user) {
+      user.getSession((err, session) => {
+        if (err) {
+          reject()
+        } else {
+          resolve(session)
+        }
+      })
+    } else {
+      reject()
+    }
+  })
+
+const loginUser = async (username, password) => {
+  var authenticationData = {
+    Username: username,
+    Password: password
+  }
+  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData)
+  var userPool = createPool()
+  var userData = {
+    Username: username,
+    Pool: userPool
+  }
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData)
+  //  let result = await asyncAuthenticateUser(cognitoUser, authenticationDetails);
+
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+      //var accessToken = result.getAccessToken().getJwtToken();
+      window.location.href = '/MyProfile'
+
+      /*
+      //POTENTIAL: Region needs to be set if not already set previously elsewhere.
+      AWS.config.region = process.env.REACT_APP_region;
+
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: userPool.userPoolId, // your identity pool id here
+        Logins: {
+          // Change the key below according to the specific region your user pool is in.
+          "cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>": result
+            .getIdToken()
+            .getJwtToken(),
+        },
+      });
+
+      //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
+      AWS.config.credentials.refresh((error) => {
+        if (error) {
+          console.error(error);
+        } else {
+          // Instantiate aws sdk service objects now that the credentials have been updated.
+          // example: var s3 = new AWS.S3();
+          console.log("Successfully logged!");
+        }
+      });
+*/
+    },
+
+    onFailure: function (err) {
+      alert(err.message || JSON.stringify(err))
+    }
+  })
+}
+
+const logoutUser = async () => {
+  var userPool = createPool()
+  const user = userPool.getCurrentUser()
+  if (user) {
+    user.signOut()
+  }
+}
+
+export { createPool, signUpUser, loginUser, logoutUser, getSession }
